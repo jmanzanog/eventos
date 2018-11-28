f1c4d553d875d41316895df5f68be351525ba61d
josemanzano <j.manzano.g@hotmail.com>
2018-11-27 20:11:49 -0500

    Integrar EventosWeb en Eventos

--- app/src/main/java/org/example/eventos/EventosWeb.java
+++ app/src/main/java/org/example/eventos/EventosWeb.java
@@ -1,10 +1,26 @@
 package org.example.eventos;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AlertDialog;
 
-public class EventosWeb extends AppCompatActivity {
+import android.support.v7.view.ContextThemeWrapper;
+import android.view.View;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+public class EventosWeb extends Activity {
     private String evento;
+    WebView navegador;
+    private ProgressDialog dialogo;
+    private ProgressBar barraProgreso;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +28,80 @@ public class EventosWeb extends AppCompatActivity {
         setContentView(R.layout.eventos_web);
         Bundle extras = getIntent().getExtras();
         evento = extras.getString("evento");
+        barraProgreso = (ProgressBar) findViewById(R.id.barraProgreso);
+        navegador = (WebView) findViewById(R.id.webkit);
+        navegador.getSettings().setJavaScriptEnabled(true);
+        navegador.getSettings().setBuiltInZoomControls(false);
+        navegador.loadUrl("https://eventos-dcda4.firebaseapp.com/index.html");
+        navegador.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                String url_filtro = "http://www.androidcurso.com/";
+                if (!url.toString().equals(url_filtro)) {
+                    view.loadUrl(url_filtro);
+                }
+                return false;
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                dialogo = new ProgressDialog(EventosWeb.this);
+                dialogo.setMessage("Cargando...");
+                dialogo.setCancelable(true);
+                dialogo.show();
+
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                //navegador.loadUrl("javascript:muestraEvento('carnaval')");
+                navegador.loadUrl("javascript:muestraEvento('"+ evento+"')");
+                navegador.loadUrl("javascript:hideOption()");
+                dialogo.dismiss();
+
+            }
+
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                AlertDialog.Builder builder =
+                        new AlertDialog.Builder(EventosWeb.this);
+                builder.setMessage(description).setPositiveButton("Aceptar", null).setTitle("onReceivedError");
+                builder.show();
+            }
+
+
+        });
+        navegador.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int progreso) {
+                barraProgreso.setProgress(0);
+                barraProgreso.setVisibility(View.VISIBLE);
+                EventosWeb.this.setProgress(progreso * 1000);
+                barraProgreso.incrementProgressBy(progreso);
+                if (progreso == 100) {
+                    barraProgreso.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message,
+                                     final JsResult result) {
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(new ContextThemeWrapper(EventosWeb.this, R.style.myDialog));
+                alertDialogBuilder.setTitle("Mensaje")
+                        .setMessage(message).setPositiveButton
+                        (android.R.string.ok, new AlertDialog.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                result.confirm();
+                            }
+                        }).setCancelable(false).create().show();
+                return true;
+            }
+
+        });
     }
+
+    private Context getContext(){
+        return this.getApplicationContext();
+    }
+
 }
